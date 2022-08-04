70fe3b4768ed9fe973ed7e17c76cd9c693029394
Jerathel Jean <jeratheljean@yahoo.com>
2022-08-03 21:42:37 -0400

    Minimum Removal To Make Valid Parentheses.

--- Code_Challenges/JavaScript/server.js
+++ Code_Challenges/JavaScript/server.js
@@ -67,5 +67,72 @@ let findMaxSlidingWindow = function(nums, windowSize) {
     console.log("-----------------------------------------------------------------------------------------------------\n")
   }
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //
+  // Remove the minimum number of parentheses, leaving matching parentheses in the input string.
+  //Given a string with matched and unmatched parentheses, 
+  //remove the minimum number of parentheses with all matching parentheses in the string.
+
+//   Solution#
+// We’ll traverse the array to:
+
+// Check whether a delimiter is unmatched or not.
+// Note the index of the unmatched delimiter so that we can remove them later on.
+// To do this, we go over each character one by one. If the character is a delimiter, 
+// we push it and its index in the stack; otherwise, we move on to the next character. 
+// To check whether the delimiter is matched or not, we make the following check at each character:
+
+// If the current character is an ending delimiter and the character at the top of the stack is a starting one, 
+// then both of them are matched.
+
+// In such a case, we do not add the current character to the stack, 
+// and we also pop an element from the stack to remove the starting delimiter. 
+// This way, we do not have any matched delimiter in the stack.
+
+// Now we make a new string and start appending characters from the end of the original string in it. 
+// We start from the end because the positions of the unmatched delimiters are at the top of the stack. 
+// In the end, we reverse the string and return it.
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  function minRemoveParentheses(s) {
+    let stack = Array().fill(Array(2).fill(null))
+
+    for (let i = 0; i < s.length; i++)
+        if (stack.length > 0 && stack[stack.length-1][1] === '(' && s.charAt(i) === ')'){
+            // closing parenthesis found while top of stack
+            // is an opening parenthesis, so pop the
+            // opening parenthesis as it's part of a pair
+            stack.pop();
+
+        }
+        else if (s.charAt(i) === '(' || s.charAt(i) === ')') {
+            // parenthesis found, push to stack for checking
+            // against the rest of the string
+            stack.push([i, s.charAt(i)]);
+        }
+
+    // At this point, the stack stores the indices
+    // that need to be removed from the input string        
+    let result = [];
+    
+    let count = 0;
+    let  c = stack.length;
+    for(let x = s.length-1; x >= 0 ; x--){
+        // compile the result string, skipping the
+        // indices that need to be removed from the input
+        if(count < c && stack[stack.length-1][0] === x){
+            stack.pop();
+            count++;
+        }
+        else
+            result.push(s.charAt(x));
+    }
+    
+    return  String(result.reverse().join(""));
+}
+
+// Driver code
+let inputs = ["11)01(110)001(", "1)01)-13(R0A1)", "11)01())-13(R(0A)1)"]
+for(let i = 0; i < inputs.length; i++){
+    console.log((i+1) +  ". Input: " + inputs[i]);
+    console.log("   Valid parentheses, after minimum removal: " +
+                minRemoveParentheses(inputs[i]));
+    console.log("-------------------------------------------------------------------------------------------------\n");
+}
