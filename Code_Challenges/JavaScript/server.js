d7653e7b970fdf21ca7f44e2b54fe823d022b86f
Jerathel Jean <jeratheljean@yahoo.com>
2022-08-04 00:00:31 -0400

    Finding Low/High Index of a Key in a Sorted Array.

--- Code_Challenges/JavaScript/server.js
+++ Code_Challenges/JavaScript/server.js
@@ -597,6 +597,75 @@ let findLeastCommonNumber = function(arr1, arr2, arr3) {
 // Space complexity#
 // The space complexity of the solution is constant, O(1)
 
+// Finding the low index of the target element
+let findLowIndex = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    let mid = Math.floor(high / 2);
+  
+    while (low <= high) {
+  
+      let midElem = nums[mid];
+  
+      // Target value is less than the middle value
+      if (midElem < target) {
+        low = mid + 1;
+      } else {
+        // Target value is greater than or equal to the middle value
+        high = mid - 1;
+      }
+      // Updating the mid value
+      mid = low + Math.floor((high - low) / 2);
+    }
+    
+    if (low < nums.length && nums[low] === target) {
+      return low;
+    }
+  
+    return -1;
+  };
+  // Finding the high index of the target element
+  let findHighIndex = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    let mid = Math.floor(high / 2);
+  
+    while (low <= high) {
+      let midElem = nums[mid];
+      
+      //Target value is less than or equal to the middle value
+      if (midElem <= target) {
+        low = mid + 1;
+      } 
+      // Target value is greater than the middle value
+      else {
+        high = mid - 1;
+      }
+      // Updating the mid value
+      mid = low + Math.floor((high - low) / 2);
+    }
+  
+    if (high === -1){
+      return high
+    }
+    
+    if (high < nums.length && nums[high] === target) {
+      return high;
+    }
+  
+    return -1;
+  };
+  
+  
+  let nums = [1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6];
+  var target = 5;
+  console.log("Original List: "+ printArray(nums) + "\n");
+  console.log("Low Index of  " + target + ": " + (findLowIndex(nums, target)));
+  console.log("High Index of " + target + ": " + (findHighIndex(nums, target)));
+  target = -2;
+  console.log("\nLow Index of  " + target + ": " + (findLowIndex(nums, target)));
+  console.log("High Index of " + target + ": " + (findHighIndex(nums, target)));
+
 
 
                                                                                                                                                                       
