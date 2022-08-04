8532ee910ce763fd4b8100e26b6a1265daac8f4d
Jerathel Jean <jeratheljean@yahoo.com>
2022-08-03 22:33:44 -0400

    Finding the Smallest Common Number.

--- Code_Challenges/JavaScript/server.js
+++ Code_Challenges/JavaScript/server.js
@@ -537,6 +537,65 @@ let binarySearch = function(nums, start, end, target) {
 // The time complexity of this solution is logarithmic, O(log n)
 // Space complexity#
 // The space complexity of this solution is logarithmic, O(log n)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                                                                   Finding the Smallest Common Number
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Given three integer arrays sorted in ascending order, return the smallest number found in all three arrays.
+//We’re given three integer arrays, each sorted in ascending order. 
+//Return the smallest number common in all three arrays. In case no number is common, return -1.
+
+// Solution 
+// The arrays are sorted in ascending order. We will use three iterators simultaneously to traverse each of the arrays. 
+// We can start traversing each array from the 0^{th}
+// index, which always has the smallest value.
+// If the values pointed to by the three iterators are equal, that is the solution. 
+// Since the arrays are sorted in ascending order, that value must be the smallest value present in all of the arrays.
+// Otherwise, we see which of the three iterators points to the smallest value and increment that iterator so that it points to the next index.
+// If any of the three iterators reaches the end of the array before we find the common number, we return -1.
+let findLeastCommonNumber = function(arr1, arr2, arr3) {
+    // Initialize starting indexes for arr1, arr2 and arr3
+    let i = 0;
+    let j = 0;
+    let k = 0;
+    
+    // Iterate through three arrays 
+    while (i < arr1.length && j < arr2.length && k < arr3.length) {
+  
+      // Finding the smallest common number
+      if (arr1[i] === arr2[j] && arr2[j] === arr3[k]) {
+        return arr1[i];
+      }
+  
+      // Let's increment the iterator
+      // for the smallest value.  
+      if (arr1[i] <= arr2[j] && arr1[i] <= arr3[k]) {
+        i++;
+      } 
+  
+      else if (arr2[j] <= arr1[i] && arr2[j] <= arr3[k]) {
+        j++;
+      }
+        
+      else if (arr3[k] <= arr1[i] && arr3[k] <= arr2[j]) {
+        k++;
+      }
+      
+    }
+  
+    return -1;
+  };
+  
+  let v1 = [6, 7, 10, 25, 30, 63, 64];
+  let v2 = [0, 4, 5, 6, 7, 8, 50];
+  let v3 = [1, 6, 10, 14];
+  console.log("Array 1: " + printArray(v1));
+  console.log("Array 2: " + printArray(v2));
+  console.log("Array 3: " + printArray(v3));
+  console.log("Least Common Number: " + findLeastCommonNumber(v1, v2, v3));
+//   Time complexity#
+// The time complexity of the solution is linear, O(n)
+// Space complexity#
+// The space complexity of the solution is constant, O(1)
 
 
 
